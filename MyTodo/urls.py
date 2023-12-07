9ed195e67029de7fa2d2f0894518926066476152
Kalu Awa <47619821+kaluawa22@users.noreply.github.com>
2023-12-07 09:17:27 -0600

    added serializer, views, router

--- MyTodo/urls.py
+++ MyTodo/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     testing branching again
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from Todo.views import TodoViewSet
+
+router = DefaultRouter()
+router.register(r'todos', TodoViewSet, basename='todo')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
